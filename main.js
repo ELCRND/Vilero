b6e3990a499a8e71fcca62b2128583cb15d28e0f
ELCRND <jwhh2a@gmail.com>
2025-03-04 14:52:51 +0300

    modified:   css/main.css 	modified:   css/main.css.map 	modified:   index.html 	modified:   main.js 	deleted:    public/fonts/Raleway-ExtraBold.ttf 	deleted:    public/fonts/Raleway-Medium.ttf 	deleted:    public/fonts/Raleway-Regular.ttf 	deleted:    public/fonts/Raleway-SemiBold.ttf 	new file:   public/img/advant/advant-hydro-767.png 	new file:   public/img/advant/advant-light-767.png 	new file:   public/img/advant/advant-material-767.png 	new file:   public/img/advant/advant-orth-767.png 	new file:   public/img/advant/advant-seat-767.png 	new file:   public/img/advant/advant-textile-767.png 	modified:   scss/_globals.scss 	modified:   scss/blocks/_advant.scss 	modified:   scss/blocks/_clients.scss 	modified:   scss/blocks/_contacts.scss 	modified:   scss/blocks/_footer.scss 	modified:   scss/blocks/_gallery.scss 	modified:   scss/blocks/_header.scss 	modified:   scss/blocks/_order.scss

--- main.js
+++ main.js
@@ -32,19 +32,29 @@ const RIGHT_COLUMN = document.querySelector(
 
 let currentSlide = 0;
 let lastScrollTime = 0;
+let tresholdOffset = 0;
 const visibleSlideQuant = 2; // минимальное количество слайдов
-const scrollDelay = 400; // задержка между прокруткой;
+const scrollDelay = 500; // задержка между прокруткой;
+const speed = 1000; // скорость прокрутки в мс
 const slideHeight = LEFT_COLUMN.children[0].clientHeight;
+const totalSlides = Math.max(
+  LEFT_COLUMN.children.length,
+  CENTER_COLUMN.children.length,
+  RIGHT_COLUMN.children.length
+);
 const gap = +window.getComputedStyle(LEFT_COLUMN).gap.replaceAll(/[a-z]/g, "");
-const tresholdOffset =
-  (CENTER_COLUMN.children.length - visibleSlideQuant) * (slideHeight + gap); // компенсация начального смещения центральной колонки
-
-CENTER_COLUMN.style.transform = `translateY(-${
-  (CENTER_COLUMN.children.length - visibleSlideQuant) * (slideHeight + gap)
-}px)`; // смещение центральной колонки вверх до двух последних элементов
 
 function scrollSlider(e) {
-  e.preventDefault();
+  const isAtEnd =
+    e.deltaY > 0 && currentSlide === totalSlides - visibleSlideQuant;
+  const isAtStart = e.deltaY < 0 && currentSlide === 0;
+
+  if (isAtEnd || isAtStart) {
+    // возврат сролла страницы при достижении крайних слайдов
+    return;
+  }
+
+  e.preventDefault(); // запрет скролла страницы
 
   const currentTime = new Date().getTime();
   if (currentTime - lastScrollTime < scrollDelay) {
@@ -63,10 +73,7 @@ function scrollSlider(e) {
 }
 
 function hadleScrollDown() {
-  currentSlide = Math.min(
-    currentSlide + 1,
-    LEFT_COLUMN.children.length - visibleSlideQuant
-  );
+  currentSlide = Math.min(currentSlide + 1, totalSlides - visibleSlideQuant);
   CENTER_COLUMN.style.transform = `translateY(${
     -tresholdOffset + currentSlide * (slideHeight + gap)
   }px)`;
@@ -79,4 +86,77 @@ function handleScrollUp() {
   }px)`;
 }
 
-GALLERY.addEventListener("wheel", scrollSlider, { passive: false });
+function initGallery() {
+  if (window.innerWidth >= 1024) {
+    GALLERY.style.transform = "none";
+    GALLERY.addEventListener("wheel", scrollSlider, { passive: false });
+
+    tresholdOffset =
+      (CENTER_COLUMN.children.length - visibleSlideQuant) * (slideHeight + gap); // компенсация начального смещения центральной колонки
+
+    [LEFT_COLUMN, CENTER_COLUMN, RIGHT_COLUMN].forEach(
+      (slider) => (slider.style.transition = `transform ${speed}ms ease-out`)
+    );
+
+    CENTER_COLUMN.style.transform = `translateY(-${
+      (CENTER_COLUMN.children.length - visibleSlideQuant) * (slideHeight + gap)
+    }px)`; // смещение центральной колонки вверх до двух последних элементов
+  } else {
+    GALLERY.removeEventListener("wheel", scrollSlider);
+
+    [LEFT_COLUMN, CENTER_COLUMN, RIGHT_COLUMN].forEach(
+      (slider) => (slider.style.transition = `none`)
+    );
+
+    CENTER_COLUMN.style.transform = `translateY(0)`;
+  }
+}
+
+initGallery();
+window.addEventListener("resize", initGallery);
+//
+//
+//
+//
+//===========================GALLERY 1023===========================
+const SLIDES = document.querySelectorAll(".gallery-images__item");
+const PREV_BTN = document.querySelector(".gallery__prev");
+const NEXT_BTN = document.querySelector(".gallery__next");
+let slideIdx = 0;
+let currentOfsset = 0;
+const total = SLIDES.length;
+
+function showSlide(index, direction) {
+  if (index >= total) {
+    slideIdx = 0;
+  } else if (index < 0) {
+    slideIdx = total - 1;
+  } else {
+    slideIdx = index;
+
+    if (direction === "r") {
+      currentOfsset += SLIDES[slideIdx - 1].offsetWidth + gap;
+    } else {
+      currentOfsset -= SLIDES[slideIdx - 1].offsetWidth + gap;
+    }
+  }
+
+  GALLERY.style.transform = `translateX(-${slideIdx * 100}%)`;
+}
+
+function nextSlide() {
+  showSlide(slideIdx + 1, "r");
+}
+
+function prevSlide() {
+  showSlide(slideIdx - 1, "l");
+}
+
+PREV_BTN.addEventListener("click", prevSlide);
+NEXT_BTN.addEventListener("click", nextSlide);
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth < 768) {
+    GALLERY;
+  }
+});
